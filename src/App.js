38bebec8db477acbfa0ef84d1234943db37aae44
Mardoniosc <Mardonio@live.com>
2020-04-14 20:14:39 -0300

    :v: Cadastrando Projetos

--- src/App.js
+++ src/App.js
@@ -15,8 +15,16 @@ function App(){
     })
   }, [])
 
-  function handleAddProject(){
-    setProjects([...projects, `Novo projeto ${Date.now()}`])    
+  async function handleAddProject(){
+    // setProjects([...projects, `Novo projeto ${Date.now()}`])
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Mardonio Costa"
+    })
+
+    const project = response.data;
+
+    setProjects([...projects, project])
   }
 
   return (
