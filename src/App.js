edb1daf8970c40d0d28a0ed0b74c4d34ab601987
Mardoniosc <Mardonio@live.com>
2020-04-14 19:48:39 -0300

    :v: Importando CSS e Imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
-import Header from './components/Header'
 
+import './App.css'
+import bgImage from './assents/background.jpeg'
+
+import Header from './components/Header'
 function App(){
 
   const [projects, setProjects] = useState(['Desenvolvimento de app', 'Front end web'])
@@ -14,6 +17,8 @@ function App(){
   return (
     <>
       <Header title="Projects" />
+
+      <img width={300} src={bgImage} alt=""/>
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
